e919fe909d9a25d71cdb1581f573a9286e516bfb
Drachinsky <fozy2772@gmail.com>
2022-08-24 13:09:38 +0300

    created 002_All_Elements

--- Domaska-2/001_Id_And_Classes/script.js
+++ Domaska-2/001_Id_And_Classes/script.js
@@ -1,15 +1,16 @@
+//добавив стилі для тфйтлів по ід
 var title = document.getElementById('id1');
-title.style.color = 'bleak';
+title.style.color = 'black';
 title.style.marginTop = '10px';
 title.style.background = 'red';
 
-
+// добавив стилі по класу
 var class1 = document.querySelector('.class1');
 class1.style.color = 'blue';
 class1.style.fontSize = '14px';
 class1.style.textDecoration = 'underline';
 
-
+// добавив новий клас до існуючого
 var myDiv = document.getElementById('myDiv');
 myDiv.classList.toggle('newclass');
 console.log(document.getElementById('myDiv').style.cssText);
--- Domaska-2/002_All_Elements/script.js
+++ Domaska-2/002_All_Elements/script.js
@@ -1,1 +1,35 @@
-var body = document.querySelectorAll('body');
+var body = document.getElementById('tag');
+var elems = body.getElementsByTagName('*');
+
+for (var i = 0; i < elems.length; i++) {
+    elems[i].style.color = 'red'
+    elems[i].style.textDecoration = 'none'
+}
+
+var elems1 = body.getElementsByTagName('p');
+for (var i = 0; i < elems1.length; i++) {
+    elems1[i].style.color = 'brown';
+    elems1[i].style.textTransform = 'uppercase';
+}
+
+var elems2 = body.getElementsByTagName('a');
+for (var i = 0; i < elems2.length; i++) {
+    elems2[i].style.fontWeight = 'bold';
+    elems2[i].style.fontStyle = 'italic';
+    elems2[i].style.textDecoration = 'underline';
+    elems2[i].style.textTransform = 'uppercase';
+}
+
+var paragraf = document.querySelector('.class');
+paragraf.style.color = 'green';
+
+var myIdClass = document.getElementById('id1');
+myIdClass.style.color = 'black'
+myIdClass.style.marginTop = '10px';
+myIdClass.style.background = 'red';
+myIdClass.classList.toggle('newclass');
+console.log(document.getElementById('id1').style.cssText);
+
+var myIdClass2 = document.getElementById('id2');
+myIdClass2.classList.toggle('newclass');
+console.log(document.getElementById('id2').style.cssText);
